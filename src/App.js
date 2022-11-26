0d1d83c0693b698dfbfd66516ff05396bb66d29b
ASUS <Yehor Kholodiuk>
2022-11-26 16:39:34 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
+  const [tank, setTank] = useState(0);
+  const [mpg, setMpg] = useState(0);
+  const [distance, setDistance] = useState(0);
+  const [result, setResult] = useState('')
+  const onTankChange = (event) => {
+  setTank(event.target.value)
+      calculate()
+  }
+
+  const onMpgChange =(event) => {
+ setMpg(event.target.value)
+      calculate()
+  }
+
+  const onDistanceChange = (event) => {
+  setDistance(event.target.value)
+      calculate()
+  }
+
+  const calculate = ( ) => {
+      const carDistance = tank * mpg;
+      if (carDistance >= distance) setResult('we arrived')
+      else
+          setResult('we did not arrive')
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <label htmlFor="tank">Tank, gal</label>
+        <input value={tank} onChange={onTankChange} name="tank" type="number"/>
+      </div>
+      <div>
+        <label htmlFor="mpg">MPG</label>
+        <input value={mpg} onChange={onMpgChange} name="mpg" type="number"/>
+      </div>
+      <div>
+        <label htmlFor="distance">Distance, miles</label>
+        <input value={distance} onChange={onDistanceChange} name="distance" type="number"/>
+
+      </div>
+
+
+        <hr/>
+        <div>{tank}</div>
+        <div>{mpg}</div>
+        <div>{distance}</div>
+
+        {result}
     </div>
   );
 }
